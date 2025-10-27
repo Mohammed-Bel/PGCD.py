66bf6c4341e02a98dff552f6eacc8588a00fca94
Med_snfx <bellahcenemohammed5@gmail.com>
2025-10-28 00:03:35 +0100

    Refactor PGCD.py for clarity and comments

--- PGCD.py
+++ PGCD.py
@@ -1,10 +1,14 @@
+# Programme pour calculer le PGCD de deux nombres
 
+# Lecture des deux nombres
 A = int(input("Entrez le premier nombre A : "))
 B = int(input("Entrez le deuxième nombre B : "))
 
+# Algorithme d'Euclide
 while B != 0:
     R = A % B
     A = B
     B = R
 
+# Affichage du résultat
 print("Le PGCD est :", A)
